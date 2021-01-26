ac8c139dc28dd07ae796fb57160b368bfd1e2924
masbys <marri.bystrova@gmail.com>
2021-01-26 21:34:28 +0300

    complited resume

--- src/main/java/ru/hh/school/entity/Resume.java
+++ src/main/java/ru/hh/school/entity/Resume.java
@@ -1,29 +1,35 @@
 package ru.hh.school.entity;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 //TODO: оформите entity
+@Entity
+@Table(name = "resume")
 public class Resume {
-  // TODO: сделать так, чтобы id брался из sequence-а
-  // таким образом, мы сможем отправлять в бд запросы батчами.
-  // нужно учитывать, что описание sequence при создании таблицы также должно соответствовать
-  // хиберовской сущности (см. create_resume.sql)
-  //
-  // Подробнее:
-  // https://vladmihalcea.com/how-to-batch-insert-and-update-statements-with-hibernate/
-  // https://vladmihalcea.com/from-jpa-to-hibernates-legacy-and-enhanced-identifier-generators/
+    // TODO: сделать так, чтобы id брался из sequence-а
+    // таким образом, мы сможем отправлять в бд запросы батчами.
+    // нужно учитывать, что описание sequence при создании таблицы также должно соответствовать
+    // хиберовской сущности (см. create_resume.sql)
+    //
+    // Подробнее:
+    // https://vladmihalcea.com/how-to-batch-insert-and-update-statements-with-hibernate/
+    // https://vladmihalcea.com/from-jpa-to-hibernates-legacy-and-enhanced-identifier-generators/
 
-  @Id
-  @GeneratedValue(/* здесь место для вашего кода */)
-  private Integer id;
+    @Id
+    @GeneratedValue(generator = "gen_resume_id_seq", strategy = SEQUENCE)
+    @SequenceGenerator(name = "gen_resume_id_seq", sequenceName = "resume_id_seq")
+    private Integer id;
 
-  private String description;
+    @Column(name = "description")
+    private String description;
 
-  Resume() {}
+    Resume() {
+    }
 
-  public Resume(String description) {
-    this.description = description;
-  }
+    public Resume(String description) {
+        this.description = description;
+    }
 
 }
