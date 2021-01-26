0363b38fc34929a6f8e988636134872cc1e93381
masbys <marri.bystrova@gmail.com>
2021-01-26 21:24:19 +0300

    completed employer

--- src/main/java/ru/hh/school/entity/Employer.java
+++ src/main/java/ru/hh/school/entity/Employer.java
@@ -1,64 +1,82 @@
 package ru.hh.school.entity;
 
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static org.hibernate.annotations.CascadeType.SAVE_UPDATE;
+
 //TODO: оформите entity
+@Entity
+@Table(name = "employer")
 public class Employer {
 
-  private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employer_id")
+    private Integer id;
 
-  private String companyName;
+    @Column(name = "company_name")
+    private String companyName;
 
-  // не используйте java.util.Date
-  // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#basic-datetime-java8
-  private LocalDateTime creationTime;
+    // не используйте java.util.Date
+    // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#basic-datetime-java8
+    @Column(name = "creation_time")
+    private LocalDateTime creationTime;
 
-  private List<Vacancy> vacancies = new ArrayList<>();
+    @OneToMany(mappedBy = "employer", fetch = FetchType.LAZY)
+    @Cascade(SAVE_UPDATE)
+    private final List<Vacancy> vacancies = new ArrayList<>();
 
-  private LocalDateTime blockTime;
+    @Column(name = "block_time")
+    private LocalDateTime blockTime;
 
-  public List<Vacancy> getVacancies() {
-    return vacancies;
-  }
+    public Employer() {
+    }
 
-  public Integer getId() {
-    return id;
-  }
+    public List<Vacancy> getVacancies() {
+        return vacancies;
+    }
 
-  public String getCompanyName() {
-    return companyName;
-  }
+    public Integer getId() {
+        return id;
+    }
 
-  public void setCompanyName(String companyName) {
-    this.companyName = companyName;
-  }
+    public String getCompanyName() {
+        return companyName;
+    }
 
-  public LocalDateTime getBlockTime() {
-    return blockTime;
-  }
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
 
-  public void setBlockTime(LocalDateTime blockTime) {
-    this.blockTime = blockTime;
-  }
+    public LocalDateTime getBlockTime() {
+        return blockTime;
+    }
 
-  // статьи на тему реализации equals() и hashCode():
-  //
-  // https://vladmihalcea.com/hibernate-facts-equals-and-hashcode/
-  // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Employer employer = (Employer) o;
-    return Objects.equals(companyName, employer.companyName);
-  }
+    public void setBlockTime(LocalDateTime blockTime) {
+        this.blockTime = blockTime;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(companyName);
-  }
+    // статьи на тему реализации equals() и hashCode():
+    //
+    // https://vladmihalcea.com/hibernate-facts-equals-and-hashcode/
+    // https://docs.jboss.org/hibernate/orm/5.3/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employer employer = (Employer) o;
+        return Objects.equals(companyName, employer.companyName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(companyName);
+    }
 
 }
